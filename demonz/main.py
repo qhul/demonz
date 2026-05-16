9b162bdfa0cf4a7da4b76f972acc9ee7b450e50a
torball <weshlua@gmail.com>
2026-05-16 15:31:59 -0700

    Update main.py

--- demonz/main.py
+++ demonz/main.py
@@ -1,39 +1,46 @@
 import requests
 from pystyle import Colorate, Colors, Center
 from colorama import Fore
+import time
 
 def main():
     banner = """
-    
+
 ██████╗ ███████╗███╗   ███╗ ██████╗ ███╗   ██╗███████╗
 ██╔══██╗██╔════╝████╗ ████║██╔═══██╗████╗  ██║╚══███╔╝
-██║  ██║█████╗  ██╔████╔██║██║   ██║██╔██╗ ██║  ███╔╝ 
-██║  ██║██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║ ███╔╝  
+██║  ██║█████╗  ██╔████╔██║██║   ██║██╔██╗ ██║  ███╔╝
+██║  ██║██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║ ███╔╝
 ██████╔╝███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║███████╗
 ╚═════╝ ╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
 
               { [+] Creator Qhul }
-                                                      
+
     """
 
     print(Colorate.Vertical(Colors.purple_to_blue, Center.XCenter(banner)))
 
-    target = input(f"{Fore.RED}[+] Enter Target Site URL:")
+    target = input(f"{Fore.RED}[+] Enter Target Site URL: ")
     amount = int(input(f"{Fore.MAGENTA}[+] Enter Amount Of Requests: "))
 
-
     for _ in range(amount):
+
+        start = time.time()
+
         response = requests.get(url=target)
 
-        if response.status_code == 200:
-            print(f"{Fore.BLUE}Requests Sent. ms{f:.2}")
+        end = time.time()
 
-        elif response.status_code == 404 or 429:
-            print("Requests Failed")
-            continue
+        ms = (end - start) * 1000
+
+        if response.status_code == 200:
+            print(f"{Fore.BLUE}[+] Request Sent | {ms:.2f} ms")
+
+        elif response.status_code == 404 or response.status_code == 429:
+            print(f"{Fore.RED}[-] Request Failed")
+
         else:
-            exit(0)
+            print(f"{Fore.YELLOW}[!] Status Code: {response.status_code}")
+
+    print(f"{Fore.GREEN}[+] Finished.")
 
 main()
-
-print(f"{Fore.GREEN} Finished.")
